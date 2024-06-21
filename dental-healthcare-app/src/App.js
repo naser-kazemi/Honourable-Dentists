63f3b696da8b4149b71b035d50582deddd42abac
dadbeh123 <dadbeh123@gmail.com>
2024-06-21 10:41:00 +0330

    Added a page for technician registration

--- dental-healthcare-app/src/App.js
+++ dental-healthcare-app/src/App.js
@@ -8,6 +8,7 @@ import Registration from "./Pages/Registration";
 import Login from "./Pages/Login";
 import PatientRegistration from "./Pages/PatientRegistration";
 import DentistRegistration from "./Pages/DentistRegistration";
+import TechnicianRegistration from "./Pages/TechnicianRegistration";
 import {AuthProvider} from './AuthContext';
 import PatientDashboard from "./Pages/PatientDashboard";
 
@@ -24,7 +25,7 @@ const App = () => {
                     <Route path="/registerlogin/login" element={<Login/>}/>
                     <Route path="/registerlogin/patient" element={<PatientRegistration/>}/>
                     <Route path="/registerlogin/dentist" element={<DentistRegistration/>}/>
-                    <Route path="/registerlogin/radiology" element={<Login/>}/>
+                    <Route path="/registerlogin/radiology" element={<TechnicianRegistration/>}/>
                     <Route path="/patientdashboard" element={<PatientDashboard/>}/>
                 </Routes>
             </Router>
