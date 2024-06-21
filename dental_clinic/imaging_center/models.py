2f8774698736a7b36ba7101a0050af63ba4c0804
dadbeh123 <dadbeh123@gmail.com>
2024-06-21 09:24:36 +0330

    Feature for imaging technicians to upload patient radiology images

--- dental_clinic/imaging_center/models.py
+++ dental_clinic/imaging_center/models.py
@@ -8,3 +8,11 @@ class ImagingCenter(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class RadiologyImage(models.Model):
+    technician = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'is_technician': True})
+    user_id = models.IntegerField()
+    image = models.ImageField(upload_to='radiology_images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
--- dental_clinic/imaging_center/views.py
+++ dental_clinic/imaging_center/views.py
@@ -1,10 +1,33 @@
 from django.views.generic import ListView, DetailView
 from .models import ImagingCenter
+from django.contrib.auth.decorators import login_required
+from .forms import RadiologyImageForm
+from django.shortcuts import render, redirect
+from django.http import HttpResponseForbidden
+
 
 class ImagingCenterListView(ListView):
     model = ImagingCenter
     template_name = 'imaging_center/list.html'
 
+
 class ImagingCenterDetailView(DetailView):
     model = ImagingCenter
     template_name = 'imaging_center/detail.html'
+
+
+@login_required
+def upload_image(request):
+    if not request.user.is_technician:
+        return HttpResponseForbidden("You are not authorized to upload images.")
+    
+    if request.method == 'POST':
+        form = RadiologyImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            radiology_image = form.save(commit=False)
+            radiology_image.technician = request.user
+            radiology_image.save()
+            return redirect('some_view')
+    else:
+        form = RadiologyImageForm()
+    return render(request, 'imaging_center/upload_image.html', {'form': form})
